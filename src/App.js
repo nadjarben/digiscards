11f55426cc3c363a30d731f4598d1580c6e08b9b
nadjarben <nadjarbenj@gmail.com>
2020-05-27 01:32:58 +0300

    add redirects

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Suspense } from "react";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
-import SuspenseUi from "./components/suspense/Suspense"
+import SuspenseUi from "./components/suspense/Suspense";
 
-import Thecleaners from "./pages/client/thecleaners/Thecleaners"
-
+import Thecleaners from "./pages/client/thecleaners/Thecleaners";
 
 function App() {
   return (
     <Suspense fallback={<SuspenseUi />}>
       <Router>
         <Switch>
-      <Route exact path="/" component={Homepage}/>
-      <Route path="/thecleaners" component={Thecleaners}/>
-      </Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/thecleaners" component={Thecleaners} />
+        </Switch>
       </Router>
     </Suspense>
   );
